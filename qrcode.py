4828d137dc6d1c8fbb8edf124486c995484c7992
Jankees Softwares <88249454+AsmInstrutor@users.noreply.github.com>
2021-09-05 08:10:06 -0300

    Add files via upload

---
+++ qrcode.py
@@ -0,0 +1,25 @@
+# Creator: Jankees Softwares
+# Date: 31/07/2021
+# Version: 1.0
+# About: This tool is used for translate link in qrcode 
+# image, this software is distributed free (code free)
+# Contact: jankees.software.66@gmail.com
+
+import os
+
+def qrcode():
+    while True:
+        import pyqrcode
+        import png
+        from pyqrcode import QRCode
+
+        QRCode = input("Link: ")
+
+        url = pyqrcode.create(QRCC)
+        url.png(r"qr.png", scale = 8)
+
+def banner():
+    os.system("figlet -f slant QRCode|lolcat")
+    qrcode()
+
+banner()
