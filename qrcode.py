f1b6822ec59bb64b9649857f6494dce3a3753fc8
Jankees Softwares <88249454+AsmInstrutor@users.noreply.github.com>
2021-09-05 08:37:05 -0300

    Update qrcode.py

--- qrcode.py
+++ qrcode.py
@@ -1,9 +1,4 @@
-# Creator: Jankees Softwares
-# Date: 31/07/2021
-# Version: 1.0
-# About: This tool is used for translate link in qrcode 
-# image, this software is distributed free (code free)
-# Contact: jankees.software.66@gmail.com
+
 
 import os
 
